cdae69ec4fa795b91621f3d1127389723b77aec1
taufanmahaputra <hacker.indo62@gmail.com>
2018-04-15 21:34:20 +0700

    make conditional program flow based on arguments

--- src/main/java/com/test/mapan/taufan/App.java
+++ src/main/java/com/test/mapan/taufan/App.java
@@ -1,5 +1,6 @@
 package com.test.mapan.taufan;
 
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -39,59 +40,32 @@ public class App
 		} 
 	}
 
-    public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-
+    public static void main(String[] args) throws FileNotFoundException {
 		App main = new App();
 
-		while(true) {
-			String command = input.nextLine();
+		String[] commands;
 
-			String[] commands = command.split(" ");
+		if (args.length > 0) {
+			Scanner scan = new Scanner(new File(args[0]));
 
-			main.processInput(commands);
+		    while(scan.hasNextLine()){
+		        String line = scan.nextLine();
+		        
+		        commands = line.split(" ");
+
+				main.processInput(commands);
+		    }
 		}
+		else {
+			Scanner input = new Scanner(System.in);
 
-		// INPUT
+			while(true) {
+				String command = input.nextLine();
 
-		// create_parking_lot 6
-		// park KA-01-HH-1234 White
-		// park KA-01-HH-9999 White
-		// park KA-01-BB-0001 Black
-		// park KA-01-HH-7777 Red
-		// park KA-01-HH-2701 Blue
-		// park KA-01-HH-3141 Black
-		// leave 4
-		// status
-		// park KA-01-P-333 White
-		// park DL-12-AA-9999 White
-		// registration_numbers_for_cars_with_colour White
-		// slot_numbers_for_cars_with_colour White
-		// slot_number_for_registration_number KA-01-HH-3141
-		// slot_number_for_registration_number MH-04-AY-1111
+				commands = command.split(" ");
 
-
-		// OUTPUT
-
-		// Created a parking lot with 6 slots
-		// Allocated slot number: 1
-		// Allocated slot number: 2
-		// Allocated slot number: 3
-		// Allocated slot number: 4
-		// Allocated slot number: 5
-		// Allocated slot number: 6
-		// Slot number 4 is free
-		// Slot No. Registration No Colour
-		// 1 KA-01-HH-1234 White
-		// 2 KA-01-HH-9999 White
-		// 3 KA-01-BB-0001 Black
-		// 5 KA-01-HH-2701 Blue
-		// 6 KA-01-HH-3141 Black
-		// Allocated slot number: 4
-		// Sorry, parking lot is full
-		// KA-01-HH-1234, KA-01-HH-9999, KA-01-P-333
-		// 1, 2, 4
-		// 6
-		// Not found
+				main.processInput(commands);
+			}
+		}
 	}
 }
