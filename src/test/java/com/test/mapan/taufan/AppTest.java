92cf8c5b887171961a38f7acaa5b4b05f01ad88b
taufanmahaputra <hacker.indo62@gmail.com>
2018-04-15 23:14:55 +0700

    remove useless import

--- src/test/java/com/test/mapan/taufan/AppTest.java
+++ src/test/java/com/test/mapan/taufan/AppTest.java
@@ -4,8 +4,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
+// import static org.hamcrest.CoreMatchers.containsString;
+// import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
