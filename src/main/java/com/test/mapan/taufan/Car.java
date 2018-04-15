093e7d774980a61e992b43a91aa8180d6ce4d6c5
taufanmahaputra <hacker.indo62@gmail.com>
2018-04-15 11:35:32 +0700

    define car class

--- src/main/java/com/test/mapan/taufan/Car.java
+++ src/main/java/com/test/mapan/taufan/Car.java
@@ -6,5 +6,19 @@ package com.test.mapan.taufan;
  */
 public class Car 
 {
-    
+    private String numberPlate;
+	private String color;
+
+	Car(String plate, String clr) {
+		numberPlate = plate;
+		color = clr;
+	}
+
+	public String getNumberPlate() {
+		return numberPlate;
+	}
+
+	public String getColor() {
+		return color;
+	}
 }
